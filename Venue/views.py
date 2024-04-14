56eef768c676fcb9a29f3d367e3ed0884105f3d6
LiYanTao <641217688tly@gmail.com>
2024-04-14 12:23:18 +0800

    Save before removing jinja2 template

--- Venue/views.py
+++ Venue/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, redirect
 
+from Venue.models import Venue
+
 
 def home(request):
-    if not request.user.is_authenticated:
-        return redirect('User:login')
-    return render(request, 'Venue/home.html')
+    if request.method == 'GET':
+        # 先获取所有的Venue
+        venues = Venue.objects.all()
+        # 然后把所有的Venue传给模板
+        return render(request, 'Venue/home.html', {'venues': venues})
+    if request.method == 'POST':
+        # 获取用户提交的数据
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        # 创建一个新的Venue
+        Venue.objects.create(name=name, address=address)
+        # 重定向到home页面
+        return redirect('home')
