5ac712cc87cec83e0584ad49ee2f54e9f19fa801
LiYanTao <641217688tly@gmail.com>
2024-04-15 15:33:29 +0800

    The Django-Crispy forms library was imported to make Django forms pretty

--- COMP3030J_Hudson/settings.py
+++ COMP3030J_Hudson/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    "crispy_bootstrap5",
     'Inventory',
     'User',
     'Statistic',
@@ -46,6 +48,10 @@ INSTALLED_APPS = [
     'Exhibition',
     'Booth'
 ]
+
+# 配置Django crispy_forms包的表单渲染模板
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
