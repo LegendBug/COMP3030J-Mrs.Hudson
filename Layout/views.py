161994304cdab1ec8a13ed20e653ca994c192143
LiYanTao <641217688tly@gmail.com>
2024-04-19 14:32:49 +0800

    Modified the function of layout module

--- Layout/views.py
+++ Layout/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from Venue.models import Venue
+
+
 def venue_layout(request):
-    return render(request, 'Layout/venue_layout.html')
+    # 从session中获取venue_id
+    venue_id = request.session.get('venue_id')
+    venue = Venue.objects.get(id=venue_id)
+    # 获取当前场馆的items
+    items = venue.items.all()
+    return render(request, 'Layout/venue_layout.html', {'venue': venue, 'items': items})
