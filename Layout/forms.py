24f6b5fa5c82189d07c5f60b326789069bcd8989
LiYanTao <641217688tly@gmail.com>
2024-05-04 13:27:20 +0800

    The layout module was refactored(ver2.0): The function of adding new layer in Layout module is completed

--- Layout/forms.py
+++ Layout/forms.py
@@ -7,14 +7,19 @@ from Venue.models import Venue
 
 
 class AddLayerForm(forms.ModelForm):
-    parent_unit = forms.ModelChoiceField(queryset=SpaceUnit.objects.all(), widget=forms.HiddenInput(), required=False)
-    floor = forms.IntegerField(widget=forms.HiddenInput())
     user_type = forms.CharField(widget=forms.HiddenInput())
-    affiliation_object_id = forms.IntegerField(widget=forms.HiddenInput())
 
     class Meta:
         model = SpaceUnit
-        fields = ['name', 'description', 'available', 'parent_unit', 'floor', 'user_type', 'affiliation_object_id']
+        fields = ['name', 'description', 'available', 'parent_unit', 'floor', 'affiliation_object_id']
+        widgets = { # widgets是一个字典，用于指定每个字段的小部件
+            'name': forms.TextInput(attrs={'placeholder': 'Layer Name'}),
+            'parent_unit': forms.HiddenInput(),
+            'available': forms.CheckboxInput(),
+            'description': forms.Textarea(attrs={'rows': 3}),
+            'floor': forms.HiddenInput(),
+            'affiliation_object_id': forms.HiddenInput(),
+        }
 
     def __init__(self, *args, **kwargs):
         super(AddLayerForm, self).__init__(*args, **kwargs)
@@ -25,7 +30,7 @@ class AddLayerForm(forms.ModelForm):
         # 保存SpaceUnit实例
         space_unit = super(AddLayerForm, self).save(commit=False)
 
-        parent_id = self.cleaned_data.get('parent_unit_id')
+        parent_unit = self.cleaned_data.get('parent_unit')
         floor = self.cleaned_data.get('floor')
         user_type = self.cleaned_data.get('user_type')
         if user_type == 'Manager':
@@ -35,7 +40,6 @@ class AddLayerForm(forms.ModelForm):
         else:
             affiliation = Booth.objects.get(id=self.cleaned_data.get('affiliation_object_id'))
 
-        parent_unit = SpaceUnit.objects.get(id=parent_id)
         space_unit.floor = floor
         space_unit.parent_unit = parent_unit
         space_unit.affiliation_content_type = ContentType.objects.get_for_model(affiliation)
