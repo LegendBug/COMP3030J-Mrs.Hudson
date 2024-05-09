3dfa9408fd7263cfa6e991dbb0d1ec5729a8a3a4
LiYanTao <641217688tly@gmail.com>
2024-05-09 12:05:39 +0800

    The layout module was refactored(ver2.7): Modify Panel and Accordion Css style

--- Statistic/models.py
+++ Statistic/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
 class Usage(models.Model):
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(null=True, blank=True)
-    item = models.ForeignKey("Inventory.Item", on_delete=models.SET_NULL, related_name='usages')
+    item = models.ForeignKey("Inventory.Item", on_delete=models.SET_NULL, related_name='usages', null=True)
     exhibition = models.ForeignKey("Exhibition.Exhibition", on_delete=models.CASCADE, related_name='usages')
     water_consumption = models.FloatField(blank=True, null=True)
     power_consumption = models.FloatField(blank=True, null=True)
