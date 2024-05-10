fed5166fc2980830cb6935a7f90fe392dc0dc837
wujiehongxu <jiehongxuw@gmail.com>
2024-05-10 22:57:12 +0800

    update statistic.views.py(finish part of work)

--- Statistic/urls.py
+++ Statistic/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('statistic/', views.statistic, name='statistic'),
+    path('get_consumption_data/<int:year>/', views.get_consumption_data, name='get_consumption_data'),
 ]
+
--- Statistic/views.py
+++ Statistic/views.py
@@ -1,9 +1,57 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from Inventory.views import get_all_venues_monthly_consumption
 
 
+def get_consumption_data(request, year):
+    # 获取整合了用水量和用电量的统计数据
+    combined_statistics = get_all_venues_monthly_consumption(year)
 
+    # 分别存储用水量和用电量的数据字典
+    water_statistics = {}
+    electric_statistics = {}
 
+    for month_data in combined_statistics:
+        month = month_data['month']
+        total_water = month_data['total_water']
+        total_power = month_data['total_power']
 
+        # 将用水量和用电量数据分别存储在对应的字典中
+        water_statistics[month] = total_water
+        electric_statistics[month] = total_power
+    # water_statistics = {
+    #     "Month 1": 1200,
+    #     "Month 2": 1600,
+    #     "Month 3": 1000,
+    #     "Month 4": 800,
+    #     "Month 5": 900,
+    #     "Month 6": 1600,
+    #     "Month 7": 2100,
+    #     "Month 8": 2200,
+    #     "Month 9": 1800,
+    #     "Month 10": 1500,
+    #     "Month 11": 1300,
+    #     "Month 12": 900,
+    # }
+    #
+    # # 生成用电量的假数据
+    # electric_statistics = {
+    #     "Month 1": 100,
+    #     "Month 2": 1600,
+    #     "Month 3": 1000,
+    #     "Month 4": 800,
+    #     "Month 5": 900,
+    #     "Month 6": 1600,
+    #     "Month 7": 210,
+    #     "Month 8": 2200,
+    #     "Month 9": 1800,
+    #     "Month 10": 150,
+    #     "Month 11": 1300,
+    #     "Month 12": 900,
+    # }
+
+    # 返回用水量和用电量的数据字典
+    return JsonResponse({'water_statistics': water_statistics, 'electric_statistics': electric_statistics})
 
 
 def statistic(request):
@@ -11,5 +59,4 @@ def statistic(request):
         else 'Organizer' if hasattr(request.user, 'organizer') \
         else 'Exhibitor' if hasattr(request.user, 'exhibitor') \
         else 'Guest'
-
     return render(request, 'Statistic/statistic.html', {'user_type': user_type})
