26cc4036b7d73e486baf270056b7d3502b090a3d
LiYanTao <641217688tly@gmail.com>
2024-05-14 21:00:39 +0800

    try to fix deployment errors

--- COMP3030J_Hudson/urls.py
+++ COMP3030J_Hudson/urls.py
@@ -1,23 +1,6 @@
-"""
-URL configuration for COMP3030J_Hudson project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, reverse
 from django.views.generic import RedirectView
-
 from Inventory.views import *
 from Layout.views import *
 from Statistic.views import *
@@ -40,7 +23,7 @@ urlpatterns = [path('', RedirectView.as_view(pattern_name='User:login', permanen
                path('Venue/', include(('Venue.urls', 'Venue'), namespace='Venue')),
                path('Exhibition/', include(('Exhibition.urls', 'Exhibition'), namespace='Exhibition')),
                path('Booth/', include(('Booth.urls', 'Booth'), namespace='Booth'))
-               ] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+               ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # 添加data/的url映射
+urlpatterns += staticfiles_urlpatterns()  # 添加static/的url映射
---
+++ gunicorn.conf.py
@@ -0,0 +1,25 @@
+import os
+
+# 绑定的 IP 和端口
+bind = '0.0.0.0:8000'
+
+# 工作模式，如同步工作模式或异步工作模式（如 gevent）
+worker_class = 'sync'  # 可以改为 'gevent' 以提高并发性能
+
+# 启动的工作进程数量,根据服务器的 CPU 核心数来调整
+workers = 2 * os.cpu_count() + 1
+
+# 最大请求量，超过此数量后将重启一个工作进程
+max_requests = 1000
+max_requests_jitter = 50  # 为 max_requests 添加抖动，防止所有工作进程同时重启
+
+# 超时配置（秒）
+timeout = 30  # 超过此时间后 Gunicorn 会杀掉工作进程
+
+# 日志文件配置
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+
+# 安全相关配置
+secure_scheme_headers = {'X-FORWARDED-PROTO': 'https'}
