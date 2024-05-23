2dbe54746a565d141a5f20d9970d178f158fee4c
LiYanTao <641217688tly@gmail.com>
2024-05-23 19:46:50 +0800

    Adjusted the statistics template

--- Statistic/urls.py
+++ Statistic/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('get_venues/', views.get_venues, name='get_venues'),
     path('get_consumption_data_by_venue/<str:venue_name>/<int:year>/', views.get_consumption_data_by_venue, name='get_consumption_data_by_venue'),
 
+    path('monitor_venue/', views.monitor_venue, name='monitor_venue'),
     path('recognize_flow/', views.recognize_flow, name='recognize_flow'),
     path('monitor/capture/<int:monitor_id>/', views.capture, name='capture'),
-    path('monitor_venue/', views.monitor_venue, name='monitor_venue'),
     path('refresh_data/', views.refresh_data, name='refresh_data'),
     path('synchronize_monitors_data/', views.synchronize_monitors_data, name='synchronize_monitors_data'),
     path('add_monitor/', views.add_monitor, name='add_monitor'),
