6d702a8c6cfdfb2bcb86f8e286dbb2d4d608f034
Peylics <lisichen_2901@outlook.com>
2024-06-07 16:52:27 +0800

    Fixed a bug caused by executing queries in AppConfig.ready()

--- User/apps.py
+++ User/apps.py
@@ -1,11 +1,16 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class UserConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "User"
 
     def ready(self):
-        from .models import GlobalSetting
-        authorization_code = 'LFobuC5UHf6CT3BlbkFJldFawYhXcw0zQ8D93sBo'
-        GlobalSetting.objects.get_or_create(key='AUTHORIZATION_CODE', defaults={'value': authorization_code})
+        # Connect the post_migrate signal to the handler
+        post_migrate.connect(create_default_global_settings, sender=self)
+
+def create_default_global_settings(sender, **kwargs):
+    from .models import GlobalSetting
+    # Ensure that the default setting is created after the migration
+    authorization_code = 'LFobuC5UHf6CT3BlbkFJldFawYhXcw0zQ8D93sBo'
+    GlobalSetting.objects.get_or_create(key='AUTHORIZATION_CODE', defaults={'value': authorization_code})
